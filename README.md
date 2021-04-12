d6a79becd5283e4478e465ab377114f25c16ebd3
Kisaragi Hiu <mail@kisaragi-hiu.com>
2021-04-13 05:55:58 +0900

    Initial commit
    Didn't think I'd make a functional prototype, but here it is

---
+++ README.md
@@ -0,0 +1,7 @@
+# A frontend for the Taiwanese government AQI data
+
+I want a simple frontend for it without all the bloat around other apps.
+
+I also want it to be a standalone app, eventually.
+
+This is my attempt.
---
+++ init.js
@@ -0,0 +1,89 @@
+"use strict";
+
+function govTimestampToISO8601(timestamp) {
+  // We can expect the format to stay constant, I think.
+  return (
+    timestamp
+      // "2021/04/13" -> "2021-04-13"
+      .replace(/\//g, "-")
+      // Use "T" as date/time separator
+      .replace(" ", "T")
+      // Explicit timezone
+      .concat("+08:00")
+  );
+}
+
+function renderUpdated(timestamp) {
+  let updated = document.getElementById("updated");
+  updated.setAttribute("datetime", govTimestampToISO8601(timestamp));
+  updated.setAttribute("title", govTimestampToISO8601(timestamp));
+  updated.innerText = timestamp;
+}
+
+// Map from key to display name
+// Key: key in API response
+// Value: array of [Display, Unit]
+// TODO: units from https://data.gov.tw/dataset/40448
+const extra_fields = {
+  "PM2.5": ["PM2.5", "μg/m3"],
+  PM10: ["PM10", "μg/m3"],
+  SO2: ["二氧化硫", "ppb"],
+  CO: ["一氧化碳", "ppm"],
+  O3: ["臭氧", "ppb"],
+  NO2: ["二氧化氮", "ppb"],
+  NOx: ["氮氧化物", "ppb"],
+  NO: ["一氧化氮", "ppb"],
+  WindSpeed: ["風速", "m/s"],
+  WindDirec: ["風向", "°"],
+};
+
+function renderData(site) {
+  let table = document.getElementById("other-meta");
+  let main_display = document.getElementById("aqi");
+  main_display.innerText = site.AQI;
+  for (const key in extra_fields) {
+    let row = table.insertRow(-1);
+    // Insert the field's display name
+    row
+      .insertCell(-1)
+      .appendChild(document.createTextNode(extra_fields[key][0]));
+    // Insert the field's value plus its unit
+    row
+      .insertCell(-1)
+      .appendChild(document.createTextNode(site[key] + extra_fields[key][1]));
+  }
+}
+
+function renderLocation(county, sitename) {
+  document.getElementById("county").innerText = county;
+  document.getElementById("station").innerText = sitename;
+}
+
+function renderMainView(site) {
+  renderUpdated(site["PublishTime"]);
+  renderData(site);
+  renderLocation(site["County"], site["SiteName"]);
+}
+
+let current_station = "楠梓";
+
+function refresh() {
+  let oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", function () {
+    let aqi_parsed = JSON.parse(this.responseText);
+    let current_site = aqi_parsed.records.filter(
+      (site) => site.SiteName == current_station
+    )[0];
+    renderMainView(current_site);
+  });
+  // oReq.addEventListener("error", function () {
+  // Switch to a failed view
+  // });
+  oReq.open(
+    "GET",
+    "https://data.epa.gov.tw/api/v1/aqx_p_432?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json"
+  );
+  oReq.send();
+}
+
+refresh();
